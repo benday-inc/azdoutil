3eaac072a3fa078f94822fa510e141ebe477aef2
benday <benday@benday.com>
2023-02-22 10:06:15 -0500

    updated the shell scripts to work on mac

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+reinstall="0";
+
+while getopts r: flag
+do
+    case "${flag}" in
+        r) reinstall=${OPTARG};;
+    esac
+done
+
+if [ "$reinstall" != "1" ]; then
+    echo "Installing..."
+else
+    echo "Reinstalling..."
+    ./uninstall.sh
+fi
+
 dotnet tool install --global --add-source ./Benday.AzureDevOpsUtil.ConsoleUi/bin/Debug azdoutil
