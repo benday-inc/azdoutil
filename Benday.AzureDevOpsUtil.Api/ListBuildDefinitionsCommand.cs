6f5564d7b1a890e39bd736073ca94ffac35ae9c7
Benjamin Day <benday@benday.com>
2025-02-06 14:08:42 -0500

    Added support to display all build definitions in a tpc.

--- Benday.AzureDevOpsUtil.Api/ListBuildDefinitionsCommand.cs
+++ Benday.AzureDevOpsUtil.Api/ListBuildDefinitionsCommand.cs
@@ -28,7 +28,13 @@ public class ListBuildDefinitionsCommand : AzureDevOpsCommandBase
         var arguments = new ArgumentCollection();
 
         arguments.AddString(Constants.ArgumentNameTeamProjectName)
-            .WithDescription("Team project name");
+            .WithDescription("Team project name").
+            AsNotRequired();
+
+        arguments.AddBoolean(Constants.ArgumentNameAllProjects)
+            .AllowEmptyValue()
+            .WithDescription("All build in all projects in this collection")
+            .AsNotRequired();
 
         arguments.AddBoolean(Constants.ArgumentNameNameOnly)
             .AllowEmptyValue()
@@ -60,11 +66,104 @@ public class ListBuildDefinitionsCommand : AzureDevOpsCommandBase
 
     protected override async Task OnExecute()
     {
-        _TeamProjectName = Arguments.GetStringValue(Constants.ArgumentNameTeamProjectName);
+        if (Arguments.HasValue(Constants.ArgumentNameAllProjects) == false &&
+            Arguments.HasValue(Constants.ArgumentNameTeamProjectName) == false)
+        {
+            throw new KnownException(
+                $"You must specify either /{Constants.ArgumentNameAllProjects} or supply a value for /{Constants.ArgumentNameTeamProjectName}.");
+        }
+        else if (Arguments.HasValue(Constants.ArgumentNameAllProjects) == true &&
+            Arguments.HasValue(Constants.ArgumentNameTeamProjectName) == true)
+        {
+            throw new KnownException(
+                $"You cannot specify both /{Constants.ArgumentNameAllProjects} and /{Constants.ArgumentNameTeamProjectName} at the same time.");
+        }
+
+        bool allProjects;
+
+        if (Arguments.GetBooleanValue(Constants.ArgumentNameAllProjects) == true)
+        {
+            _TeamProjectName = string.Empty;
+            allProjects = true;
+        }
+        else
+        {
+            _TeamProjectName = Arguments.GetStringValue(Constants.ArgumentNameTeamProjectName);
+            allProjects = false;
+        }
 
         var toJson = Arguments.GetBooleanValue(Constants.CommandArgumentNameToJson);
 
-        var results = await GetResult();
+        if (allProjects == false)
+        {
+            await ListForSingleProject(toJson, _TeamProjectName);
+        }
+        else
+        {
+            await ListForAllProjects(toJson);
+        }
+    }
+    private async Task ListForAllProjects(bool json)
+    {
+        // call ListTeamProjectsCommand
+        var command = new ListTeamProjectsCommand(
+            ExecutionInfo.GetCloneOfArguments(
+               Constants.CommandName_ListProcessTemplates, true), _OutputProvider);
+
+        await command.ExecuteAsync();
+
+        if (command.LastResult == null)
+        {
+            throw new KnownException("No team projects found.");
+        }
+        else
+        {
+            var teamProjects = command.LastResult.Projects;
+
+            var results = new List<BuildDefinitionInfo>();
+
+            foreach (var teamProject in teamProjects)
+            {
+                var result = await GetResult(teamProject.Name);
+
+                if (result != null && result.Count > 0)
+                {
+                    results.AddRange(result);
+                }
+            }
+
+            if (json == true)
+            {
+                WriteLine(SerializeObjectToJson(results));
+            }
+            else
+            {
+                WriteLine(String.Empty);
+                WriteLine($"Result count: {results.Count}");
+
+                var groupedResults = results.GroupBy(x => x.Project.Name);
+
+                // order by team project name
+                groupedResults = groupedResults.OrderBy(x => x.Key);
+
+                foreach (var groupedResult in groupedResults)
+                {
+                    WriteLine();
+                    WriteLine("** Team Project: " + groupedResult.Key);
+
+                    foreach (var item in groupedResult)
+                    {
+                        WriteLine(ToString(item));
+                    }
+                }
+            }
+        }
+
+    }
+
+    private async Task ListForSingleProject(bool toJson, string teamProjectName)
+    {
+        var results = await GetResult(teamProjectName);
 
         if (results == null)
         {
@@ -85,18 +184,18 @@ public class ListBuildDefinitionsCommand : AzureDevOpsCommandBase
         }
     }
 
-    private async Task<List<BuildDefinitionInfo>?> GetResult()
+    private async Task<List<BuildDefinitionInfo>?> GetResult(string teamProjectName)
     {
         string requestUrl;
 
         if (Arguments.GetBooleanValue(Constants.ArgumentNameXaml) == true)
         {
             WriteLine("** GETTING XAML BUILD DEFINITIONS **");
-            requestUrl = $"{_TeamProjectName}/_apis/build/definitions?api-version=2.2";
+            requestUrl = $"{teamProjectName}/_apis/build/definitions?api-version=2.2";
         }
         else
         {
-            requestUrl = $"{_TeamProjectName}/_apis/build/definitions?api-version=7.1";
+            requestUrl = $"{teamProjectName}/_apis/build/definitions?api-version=7.1";
         }
 
         var result = await CallEndpointViaGetAndGetResult<BuildDefinitionInfoResponse>(requestUrl);
